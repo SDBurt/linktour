91cd977d6b3499379f119026f0bd6312c27939fd
sdburt <seandburt@gmail.com>
2023-05-25 14:31:16 -0700

    remove email login until auth if flushed out

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -33,18 +33,18 @@ export default function LoginPage() {
             Welcome back
           </h1>
           <p className="text-sm text-muted-foreground">
-            Enter your email to sign in to your account
+            Sign in to your account
           </p>
         </div>
         <UserAuthForm />
-        <p className="px-8 text-center text-sm text-muted-foreground">
+        {/* <p className="px-8 text-center text-sm text-muted-foreground">
           <Link
             href="/register"
             className="hover:text-brand underline underline-offset-4"
           >
             Don&apos;t have an account? Sign Up
           </Link>
-        </p>
+        </p> */}
       </div>
     </div>
   )
