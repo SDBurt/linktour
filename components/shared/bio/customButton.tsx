e0e9a7058977d8f8d992f386829f1d5a6b28ed2a
sdburt <seandburt@gmail.com>
2023-06-05 14:30:41 -0700

    feat: add hover effect to buttons

--- components/shared/bio/customButton.tsx
+++ components/shared/bio/customButton.tsx
@@ -6,6 +6,40 @@ import { Link } from "@prisma/client"
 import { ThemeProps } from "@/lib/types"
 import { cn } from "@/lib/utils"
 
+interface BioButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string
+  url: string
+  slug: string
+  key: string
+}
+
+function BioButton({
+  slug,
+  key,
+  url,
+  title,
+  className,
+  style,
+}: BioButtonProps) {
+  async function buttonClickedHandler(s: string, k: string) {
+    if (s && k) {
+      await fetch(`/api/projects/${s}/links/${k}/click`)
+    } else {
+      console.warn("slug or link is undefined")
+    }
+  }
+
+  return (
+    <div onClick={(e) => buttonClickedHandler(slug, key)}>
+      <NextLink href={url} className={className} style={style} target="_blank">
+        <div>
+          <h1>{title}</h1>
+        </div>
+      </NextLink>
+    </div>
+  )
+}
+
 interface CustomButtonProps {
   link: Pick<Link, "title" | "url" | "slug" | "key">
   theme: ThemeProps
@@ -13,18 +47,23 @@ interface CustomButtonProps {
 
 const makeGeneralStyle = (buttonType: string) => {
   const customStyles = {
-    FILL: "",
-    FILL_ROUNDED: "rounded-lg",
-    FILL_CIRCULAR: "rounded-full",
-    OUTLINE: "border",
-    OUTLINE_ROUNDED: "border rounded-lg",
-    OUTLINE_CIRCULAR: "border rounded-full",
-    SOFTSHADOW: "shadow",
-    SOFTSHADOW_ROUNDED: "shadow rounded-lg",
-    SOFTSHADOW_CIRCULAR: "shadow rounded-full",
-    HARDSHADOW: "hard-shadow",
-    HARDSHADOW_ROUNDED: "hard-shadow rounded-lg",
-    HARDSHADOW_CIRCULAR: "hard-shadow rounded-full",
+    FILL: "hover:bg-transparent hover:translate-y-1",
+    FILL_ROUNDED: "rounded-lg hover:bg-transparent hover:translate-y-1",
+    FILL_CIRCULAR: "rounded-full hover:bg-transparent hover:translate-y-1",
+    OUTLINE: "border hover:scale-[1.01]",
+    OUTLINE_ROUNDED: "border rounded-lg hover:scale-[1.01]",
+    OUTLINE_CIRCULAR: "border rounded-full hover:scale-[1.01]",
+    SOFTSHADOW: "shadow hover:drop-shadow-sm hover:scale-[1.01]",
+    SOFTSHADOW_ROUNDED:
+      "shadow rounded-lg hover:drop-shadow-sm hover:scale-[1.01]",
+    SOFTSHADOW_CIRCULAR:
+      "shadow rounded-full hover:drop-shadow-sm hover:scale-[1.01]",
+    HARDSHADOW:
+      "hard-shadow hover:shadow-[0_4px_0px_rgb(0,0,0)] hover:-translate-y-1",
+    HARDSHADOW_ROUNDED:
+      "hard-shadow rounded-lg hover:shadow-[0_4px_0px_rgb(0,0,0)] hover:-translate-y-1",
+    HARDSHADOW_CIRCULAR:
+      "hard-shadow rounded-full hover:shadow-[0_4px_0px_rgb(0,0,0)] hover:-translate-y-1",
   }
 
   return `${customStyles[buttonType]}`
@@ -38,42 +77,49 @@ const isOutline = (buttonType: string) => {
   )
 }
 
+const makeBackgroundStyle = (type: string, color: string) => {
+  if (isOutline(type)) {
+    return undefined
+  }
+  return color
+}
+
+const makeBorderStyle = (type: string, color: string) => {
+  if (isOutline(type)) {
+    return undefined
+  }
+  return color
+}
+
 export default function CustomButton({ link, theme }: CustomButtonProps) {
+  // we can use tailwind for non-arbitrary values
   const buttonCls = cn(
-    "flex h-full w-full items-center justify-center p-4 font-medium",
+    "flex h-full w-full items-center justify-center p-4 font-medium", // position and base style
+    "ease-out transition-all", // animate
     makeGeneralStyle(theme?.buttonType)
   )
 
+  // arbitrary values can't dynamically be set so we have to use style
   const buttonStyle = {
-    backgroundColor: isOutline(theme?.buttonType)
-      ? undefined
-      : theme.buttonBackgroundColor,
-    borderColor: isOutline(theme?.buttonType)
-      ? theme.buttonBackgroundColor
-      : undefined,
+    backgroundColor: makeBackgroundStyle(
+      theme?.buttonType,
+      theme.buttonBackgroundColor
+    ),
+    borderColor: makeBorderStyle(
+      theme?.buttonType,
+      theme.buttonBackgroundColor
+    ),
     color: theme.buttonTextColor,
   }
 
-  async function buttonClickedHandler(link) {
-    if (link.slug && link.key) {
-      await fetch(`/api/projects/${link.slug}/links/${link.key}/click`)
-    } else {
-      console.warn("slug or link is undefined")
-    }
-  }
-
   return (
-    <div onClick={(e) => buttonClickedHandler(link)}>
-      <NextLink
-        href={link.url}
-        className={buttonCls}
-        style={buttonStyle}
-        target="_blank"
-      >
-        <div>
-          <h1>{link.title}</h1>
-        </div>
-      </NextLink>
-    </div>
+    <BioButton
+      title={link.title}
+      url={link.url}
+      slug={link.slug}
+      key={link.key}
+      className={buttonCls}
+      style={buttonStyle}
+    />
   )
 }
