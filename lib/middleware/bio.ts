469007d3e1903fb8f3133c8c54b876c6db9ac1ac
sdburt <seandburt@gmail.com>
2023-06-09 16:47:48 -0700

    fix: revert update to recordClick

--- lib/middleware/bio.ts
+++ lib/middleware/bio.ts
@@ -8,23 +8,13 @@ import {
 import { parse } from "@/lib/middleware/utils"
 import { recordClick } from "@/lib/tinybird"
 
-import { db } from "../db"
-
 export const BioMiddleware: NextMiddleware = async (
   req: NextRequest,
   ev: NextFetchEvent
 ) => {
   const { key } = parse(req)
   if (key) {
-    const count = await db.project.count({
-      where: {
-        slug: key,
-      },
-    })
-
-    if (count > 0) {
-      ev.waitUntil(recordClick(key, req))
-    }
+    ev.waitUntil(recordClick(key, req))
   }
   return NextResponse.next()
 }
